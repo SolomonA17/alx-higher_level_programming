94affebc40f47ddbdafb9315a8ee65a34489b03f
solomon <solomonaberra75@gmail.com>
2023-11-28 09:00:25 -0800

    0x01-python-if_else_loops_functions updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if (char != 101 and char != 113):
-        print("{:c}".format(char), end='')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
