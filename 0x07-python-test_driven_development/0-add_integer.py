3dc8672c24b7afffdeda1a8b94a68435b49feef0
Solomon Aberra <solomonaberra75@gmail.com>
2024-01-10 02:59:52 -0800

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Module containing a dummy adder function for testing"""
+""" 0-add_integer Module """
+
 
 def add_integer(a, b=98):
-    """ adds integers
-        Arguments:
-        @a: first integer
-        @b: second integer, defaults to 98 if not given
     """
+    Adds two integers
 
-    if type(a) not in [int, float]:
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        addition of two integers
+    """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    else:
+        new_a, new_b = int(a), int(b)
+        return new_a + new_b
