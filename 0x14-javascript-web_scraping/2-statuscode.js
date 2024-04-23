f70f4c1889901fc412d86f91f50809d3ae5381be
Solomon Aberra <solomonaberra75@gmail.com>
2024-04-23 04:00:48 -0700

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-// Status Of Get request
+/* Displays the status code of a GET request */
 
 const request = require('request');
 const url = process.argv[2];
-request.get(url, function (error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('code:' + ' ' + response.statusCode);
-  }
-});
 
+request(url, (err, response) => {
+  if (err) console.log(err);
+  else console.log('code:', response.statusCode);
+});
